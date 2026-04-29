5f0d86fee0922aa56228b1165abbc846cc4144d3
Jordan Robinson <hello@jordanrobinson.org>
2026-04-29 20:11:40 +0100

    fix: ambiguous declaration

--- Sources/App/Controllers/VariableHourly.swift
+++ Sources/App/Controllers/VariableHourly.swift
@@ -521,23 +521,6 @@ extension GenericDomain {
             }
             return (nil, nil, nil, nil)
         }
-        return VariableHourlyDeriver<GenericReaderCached<Self, Variable>>(reader: GenericReaderCached(reader: reader), options: options)
-    }
-    
-    /// Make a default reader for a single domain with hourly data
-    func makeHourlyReader<Variable: GenericVariable & Hashable>(variableType: Variable.Type, lat: Float, lon: Float, elevation: Float, mode: GridSelectionMode, options: GenericReaderOptions) async throws -> (any GenericReaderProtocol)? {
-        return try await GenericReader<Self, Variable>(domain: self, lat: lat, lon: lon, elevation: elevation, mode: mode, options: options)
-    }
-    
-    /// Make a default reader for a single domain with hourly data and inject a daily deriver
-    func makeGenericHourlyDaily<Variable: GenericVariable & Hashable>(variableType: Variable.Type, lat: Float, lon: Float, elevation: Float, mode: GridSelectionMode, options: GenericReaderOptions, throwOnMissingLocation: Bool) async throws -> (hourly: (any GenericReaderOptionalProtocol<ForecastVariable>)?, daily: (any GenericReaderOptionalProtocol<ForecastVariableDaily>)?, weekly: (any GenericReaderOptionalProtocol<ForecastVariableWeekly>)?, monthly: (any GenericReaderOptionalProtocol<ForecastVariableMonthly>)?) {
-        
-        guard let reader = try await GenericReader<Self, Variable>(domain: self, lat: lat, lon: lon, elevation: elevation, mode: mode, options: options) else {
-            if throwOnMissingLocation {
-                throw ForecastApiError.noDataAvailableForThisLocation
-            }
-            return (nil, nil, nil, nil)
-        }
         let hourly = VariableHourlyDeriver<GenericReaderCached<Self, Variable>>(reader: GenericReaderCached(reader: reader), options: options)
         return (hourly, hourly.makeDailyAggregator(allowMinMaxTwoAggregations: true), nil, nil)
     }
